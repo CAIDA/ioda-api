55723e6a44083dc29d64555315e5822c039c0c37
Alistair King <alistair@caida.org>
2018-05-23 14:32:39 -0700

    Flesh out documentation for expression validation endpoint

--- src/Controller/ExpressionController.php
+++ src/Controller/ExpressionController.php
@@ -8,6 +8,7 @@ use Nelmio\ApiDocBundle\Annotation\Model;
 use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -20,6 +21,11 @@ use Symfony\Component\Serializer\SerializerInterface;
 class ExpressionController extends Controller
 {
     /**
+     * List expression functions
+     *
+     * Gets a list of supported transformation function prototypes. Useful for
+     * presenting available functions in an expression builder UI.
+     *
      * @Route("/functions/", methods={"GET"}, name="functions")
      * @SWG\Tag(name="Expression")
      * @SWG\Response(
@@ -51,6 +57,7 @@ class ExpressionController extends Controller
      * @var Request $request
      * @var SerializerInterface $serializer
      * @var Registry $functionRegistry
+     *
      * @return JsonResponse
      */
     public function functions(Request $request, SerializerInterface $serializer, Registry $functionRegistry)
@@ -65,17 +72,92 @@ class ExpressionController extends Controller
     }
 
     /**
+     * Validate expression
+     *
+     * Validates the given JSON-formatted expression object for syntactic
+     * correctness.
+     *
      * @Route("/validate/", methods={"POST"}, name="validate")
      * @SWG\Tag(name="Expression")
+     * @SWG\Parameter(
+     *     name="expression",
+     *     in="body",
+     *     type="object",
+     *     description="JSON-encoded expression object",
+     *     required=true,
+     *     @SWG\Schema(ref=@Model(type=\App\Expression\AbstractExpression::class, groups={"public"}))
+     * )
      * @SWG\Response(
      *     response=200,
-     *     description="Indicates that the given expression was validated successfully"
+     *     description="Indicates that the given expression was validated successfully. The validated expression is also returned in the response.",
+     *     @SWG\Schema(
+     *         allOf={
+     *             @SWG\Schema(ref=@Model(type=Envelope::class, groups={"public"})),
+     *             @SWG\Schema(
+     *                 @SWG\Property(
+     *                     property="type",
+     *                     type="string",
+     *                     enum={"expression.validate"}
+     *                 ),
+     *                 @SWG\Property(
+     *                     property="error",
+     *                     type="string",
+     *                     enum={}
+     *                 ),
+     *                 @SWG\Property(
+     *                     property="data",
+     *                     type="object",
+     *                     @SWG\Schema(ref=@Model(type=\App\Expression\AbstractExpression::class, groups={"public"}))
+     *                 )
+     *             )
+     *         }
+     *     )
      * )
+     * @SWG\Response(
+     *     response=400,
+     *     description="Indicates that the given expression could not be validated.",
+     *     @SWG\Schema(
+     *         allOf={
+     *             @SWG\Schema(ref=@Model(type=Envelope::class, groups={"public"})),
+     *             @SWG\Schema(
+     *                 @SWG\Property(
+     *                     property="type",
+     *                     type="string",
+     *                     enum={"expression.validate"}
+     *                 ),
+     *                 @SWG\Property(
+     *                     property="error",
+     *                     type="string",
+     *                     enum={"Validation error: unknown function 'foo'"}
+     *                 ),
+     *                 @SWG\Property(
+     *                     property="data",
+     *                     type="string",
+     *                     enum={}
+     *                 )
+     *             )
+     *         }
+     *     )
+     * )
+     *
+     * @var Request $request
+     * @var SerializerInterface $serializer
+     *
+     * @return JsonResponse
      */
-    public function validate()
+    public function validate(Request $request, SerializerInterface $serializer)
     {
-        return $this->json([
-            'validate'
-        ]);
+        $rawExpression = $request->attributes->get('parsedContent');
+        $envelope = new Envelope(
+            'expression.validate',
+            $rawExpression,
+            $request->server->get('REQUEST_TIME')
+        );
+        if (!is_array($rawExpression)) {
+            throw new BadRequestHttpException('Missing expression');
+        }
+        // TODO: attempt to create expression from $rawExpression
+        $envelope->setData(true);
+        return $this->json($envelope);
     }
 }
