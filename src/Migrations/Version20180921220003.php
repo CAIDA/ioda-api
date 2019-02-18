9b6d46cf33ae6169d6a1ba36113670a98051d74c
Alistair King <alistair@caida.org>
2019-02-18 15:22:47 -0800

    Fix type of sym_url id column

--- src/Migrations/Version20180921220003.php
+++ src/Migrations/Version20180921220003.php
@@ -15,7 +15,7 @@ final class Version20180921220003 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE TABLE sym_url (id INT NOT NULL, short_tag VARCHAR(255) NOT NULL, long_url TEXT NOT NULL, use_count INT NOT NULL, date_created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, date_last_used TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE sym_url (id SERIAL NOT NULL, short_tag VARCHAR(255) NOT NULL, long_url TEXT NOT NULL, use_count INT NOT NULL, date_created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, date_last_used TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_953A797574C9F71C ON sym_url (short_tag)');
         $this->addSql('CREATE INDEX long_url_idx ON sym_url (long_url)');
     }
