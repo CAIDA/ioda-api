659084bfb8820f49a05402401ca531fe7cbf4f84
Mingwei Zhang <mingwei@mwzhang.com>
2020-06-26 15:52:34 -0700

    revise+cleanup /topo based on new api spec

--- src/Controller/TopoController.php
+++ src/Controller/TopoController.php
@@ -19,191 +19,40 @@ use Symfony\Component\Serializer\SerializerInterface;
 class TopoController extends ApiController
 {
     /**
-     * List available topographic databases
-     *
-     * @Route("/databases/", methods={"GET"}, name="databases")
-     * @SWG\Tag(name="Topographic")
-     * @SWG\Response(
-     *     response=200,
-     *     description="Returns a list of available topographic databases",
-     *     @SWG\Schema(
-     *         allOf={
-     *             @SWG\Schema(ref=@Model(type=Envelope::class, groups={"public"})),
-     *             @SWG\Schema(
-     *                 @SWG\Property(
-     *                     property="type",
-     *                     type="string",
-     *                     enum={"topo.databases"}
-     *                 ),
-     *                 @SWG\Property(
-     *                     property="error",
-     *                     type="string",
-     *                     enum={}
-     *                 ),
-     *                 @SWG\Property(
-     *                     property="data",
-     *                     type="array",
-     *                     items=@SWG\Property(type="string")
-     *                 )
-     *             )
-     *         }
-     *     )
-     * )
-     * @var Request $request
-     * @var SerializerInterface
-     * @var TopoService $topoService
-     * @return JsonResponse
-     */
-    public function databases(Request $request, SerializerInterface $serializer,
-                              TopoService $topoService)
-    {
-        $env = new Envelope('topo.databases',
-                            'query',
-                            [],
-                            $request
-        );
-        if ($env->getError()) {
-            return $this->json($env, 400);
-        }
-        $env->setData($topoService->getDatabases());
-        return $this->json($env);
-    }
-
-    /**
      * Get topographic database information
      *
-     * @Route("/databases/{db}/",
+     * @Route("/{entityType}",
      *     methods={"GET"},
-     *     name="database")
+     *     name="get")
      * @SWG\Tag(name="Topographic")
      * @SWG\Response(
      *     response=200,
      *     description="Returns information about the given topographic database"
      * )
-     */
-    /*
-    public function database($db)
-    {
-        return $this->json([
-            "database info for $db",
-        ]);
-    }
-    */
-
-    /**
-     * List available tables for the given topographic database
-     *
-     * @Route("/databases/{db}/tables/",
-     *     methods={"GET"},
-     *     name="database_tables")
-     * @SWG\Tag(name="Topographic")
-     * @SWG\Response(
-     *     response=200,
-     *     description="Returns a list of the available tables for the given topographic database",
-     *     @SWG\Schema(
-     *         allOf={
-     *             @SWG\Schema(ref=@Model(type=Envelope::class, groups={"public"})),
-     *             @SWG\Schema(
-     *                 @SWG\Property(
-     *                     property="type",
-     *                     type="string",
-     *                     enum={"topo.tables"}
-     *                 ),
-     *                 @SWG\Property(
-     *                     property="error",
-     *                     type="string",
-     *                     enum={}
-     *                 ),
-     *                 @SWG\Property(
-     *                     property="data",
-     *                     type="array",
-     *                     items=@SWG\Property(type="string")
-     *                 )
-     *             )
-     *         }
-     *     )
-     * )
-     *
-     * @var string $db
+     * @var string $entityType
      * @var Request $request
      * @var SerializerInterface $serializer
      * @var TopoService $topoService
      * @return JsonResponse
      */
-    public function tables(string $db, Request $request,
-                           SerializerInterface $serializer,
-                           TopoService $topoService)
-    {
-        $env = new Envelope('topo.tables',
-                            'query',
-                            [],
-                            $request
-        );
-        if ($env->getError()) {
-            return $this->json($env, 400);
-        }
-        try {
-            $env->setData($topoService->getTables($db));
-        } catch (\InvalidArgumentException $ex) {
-            $env->setError($ex->getMessage());
-            return $this->json($env, 400);
-        }
-        return $this->json($env);
-    }
-
-    /**
-     * Get TopoJSON for the given database table
-     *
-     * @Route("/databases/{db}/tables/{table}/",
-     *     methods={"GET"},
-     *     name="database_table")
-     * @SWG\Tag(name="Topographic")
-     * @SWG\Response(
-     *     response=200,
-     *     description="Returns the TopoJSON data for the given database table",
-     *     @SWG\Schema(
-     *         allOf={
-     *             @SWG\Schema(ref=@Model(type=Envelope::class, groups={"public"})),
-     *             @SWG\Schema(
-     *                 @SWG\Property(
-     *                     property="type",
-     *                     type="string",
-     *                     enum={"topo.topojson"}
-     *                 ),
-     *                 @SWG\Property(
-     *                     property="error",
-     *                     type="string",
-     *                     enum={}
-     *                 ),
-     *                 @SWG\Property(
-     *                     property="data"
-     *                 )
-     *             )
-     *         }
-     *     )
-     * )
-     *
-     * @var string $db
-     * @var string $table
-     * @var Request $request
-     * @var SerializerInterface $serializer
-     * @var TopoService $topoService
-     * @return JsonResponse
-     */
-    public function table(string $db, string $table, Request $request,
+    public function topoLookup(string $entityType, Request $request,
                           SerializerInterface $serializer,
                           TopoService $topoService)
     {
         $env = new Envelope('topo.topojson',
-                            'query',
-                            [],
-                            $request
+            'query',
+            [],
+            $request
         );
         if ($env->getError()) {
             return $this->json($env, 400);
         }
         try {
-            $env->setData($topoService->getTopoJson($db, $table));
+            $env->setData([
+                "entityType" => $entityType,
+                "idField" => $topoService->getIdField($entityType),
+                "topology" => $topoService->getTopoJson($entityType)
+            ]);
         } catch (\InvalidArgumentException $ex) {
             $env->setError($ex->getMessage());
             return $this->json($env, 400);
--- src/Topo/TopoService.php
+++ src/Topo/TopoService.php
@@ -7,51 +7,29 @@ class TopoService
 {
     const DB_PATH_PFX = '/var/topojson';
 
-    const DATABASES = [
-        'natural-earth' => [
-            'ne_10m_admin_0.continents.v3.1.0',
-            'ne_10m_admin_0.countries.v3.1.0',
-            'ne_10m_admin_1.regions.v3.0.0',
-        ],
-        'gadm' => [
-            'gadm.counties.v2.0',
-        ],
+    const ENTITY_TYPE_TO_DB = [
+        "continent" => ["natural-earth", "ne_10m_admin_0.continents.v3.1.0"],
+        "country" => ["natural-earth", "ne_10m_admin_0.countries.v3.1.0"],
+        "region" => ["natural-earth", "ne_10m_admin_1.regions.v3.0.0"],
+        "county" => ["gadm", "gadm.counties.v2.0"],
     ];
 
-    /**
-     * Get a list of supported databases.
-     *
-     * @return string[]
-     */
-    public function getDatabases(): array
-    {
-        return array_keys(TopoService::DATABASES);
-    }
+    const ENTITY_TYPE_TO_ID_FIELD = [
+        "continent" => "usercode",
+        "country" => "usercode",
+        "region" => "id",
+        "county" => "id",
+    ];
 
-    /**
-     * Get a list of tables for the given database.
-     *
-     * @var string $db
-     * @return string[]
-     */
-    public function getTables(string $db): array
+    public function getTopoJson(string $entityType): array
     {
-        if (!array_key_exists($db, TopoService::DATABASES)) {
-            throw new \InvalidArgumentException("Invalid database '$db'");
-        }
-        return TopoService::DATABASES[$db];
-    }
-
-    public function getTopoJson(string $db, string $table): array
-    {
-        if (!array_key_exists($db, TopoService::DATABASES)) {
-            throw new \InvalidArgumentException("Invalid database '$db'");
-        }
-        if (!in_array($table, TopoService::DATABASES[$db])) {
-            throw new \InvalidArgumentException("Invalid table '$table'");
+        if (!array_key_exists($entityType, self::ENTITY_TYPE_TO_DB)) {
+            throw new \InvalidArgumentException("Invalid entity type '$entityType'");
         }
         // build the file path
         // TODO move this data to swift?
+        $db = self::ENTITY_TYPE_TO_DB[$entityType][0];
+        $table = self::ENTITY_TYPE_TO_DB[$entityType][1];
         $file = implode('/', [TopoService::DB_PATH_PFX, $db, $table]) . '.processed.topo.json';
         if (!file_exists($file)) {
             throw new \InvalidArgumentException("Could not load TopoJson for $db/$table ($file)");
@@ -59,5 +37,12 @@ class TopoService
         // TODO: if this is too slow, remove envelope and return file contents
         // TODO: directly to save the json decode/encode step
         return json_decode(file_get_contents($file), true);
+    }
+    public function getIdField(string $entityType): string
+    {
+        if (!array_key_exists($entityType, self::ENTITY_TYPE_TO_ID_FIELD)) {
+            throw new \InvalidArgumentException("Invalid entity type '$entityType'");
+        }
+        return self::ENTITY_TYPE_TO_ID_FIELD[$entityType];
     }
 }
