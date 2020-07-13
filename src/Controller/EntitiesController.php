cdd99581887202170b448f064c2b9d64dd253dca
Mingwei Zhang <mingwei@mwzhang.com>
2020-07-13 15:45:50 -0700

    add swagger doc for entities lookup endpoint

--- src/Controller/EntitiesController.php
+++ src/Controller/EntitiesController.php
@@ -22,7 +22,69 @@ use Symfony\Component\Serializer\SerializerInterface;
 class EntitiesController extends ApiController
 {
     /**
+     * Lookup metadata entities
+     *
+     * Returns a JSON object with metadata for the searched entities.
+     *
      * @Route("/{entityType}/{entityCode}", methods={"GET"}, name="get", defaults={"entityType"=null,"entityCode"=null})
+     * @SWG\Tag(name="Metadata Entities")
+     * @SWG\Parameter(
+     *     name="entityType",
+     *     in="path",
+     *     type="string",
+     *     description="Type of the entity, e.g. country, region, asn",
+     *     default=null
+     * )
+     * @SWG\Parameter(
+     *     name="entityCode",
+     *     in="path",
+     *     type="string",
+     *     description="Code of the entity, e.g. for United States the code is 'US'",
+     *     required=false,
+     *     default=null
+     * )
+     * @SWG\Parameter(
+     *     name="relatedTo",
+     *     in="query",
+     *     type="string",
+     *     description="Find entities related to another entity. Format: 'entityType[/entityCode]",
+     *     required=false,
+     * )
+     * @SWG\Parameter(
+     *     name="search",
+     *     in="query",
+     *     type="string",
+     *     description="Search entities with name that matches the search term",
+     *     required=false,
+     * )
+     * @SWG\Response(
+     *     response=200,
+     *     description="Return an array of metadata entities",
+     *     @SWG\Schema(
+     *         allOf={
+     *             @SWG\Schema(ref=@Model(type=Envelope::class, groups={"public"})),
+     *             @SWG\Schema(
+     *                 @SWG\Property(
+     *                     property="type",
+     *                     type="string",
+     *                     enum={"sym.get"}
+     *                 ),
+     *                 @SWG\Property(
+     *                     property="error",
+     *                     type="string",
+     *                     enum={}
+     *                 ),
+     *                 @SWG\Property(
+     *                     property="data",
+     *                     type="array",
+     *                     @SWG\Items(
+     *                          ref=@Model(type=\App\Entity\Ioda\MetadataEntity::class, groups={"public"})
+     *                     )
+     *                 )
+     *             )
+     *         }
+     *     )
+     * )
      *
      * @var string|null $entityType
      * @var string|null $entityCode
